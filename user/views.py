1c2c9525a4bef47582f875292d47ae2746b2e4d0
Pratik Joshi <expertnoob@expertnoob.local>
2024-11-10 20:25:39 +0545

    minor changes

--- user/views.py
+++ user/views.py
@@ -4,10 +4,10 @@ from rest_framework import viewsets
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAuthenticated
 
-User = get_user_model()  # Use get_user_model to get the custom user model
+User = get_user_model() 
 
 
-class UserViewSet(viewsets.ReadOnlyModelViewSet):  # Read-only for listing and retrieving user info
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
